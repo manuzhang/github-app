bd8a9f9b6e797c8135d8088321cd065928a9ca7b
manuzhang <owenzhang1990@gmail.com>
2019-11-30 10:47:51 +0800

    Add example of querying repo project

---
+++ src/main/scala/io/github/manuzhang/graphql/GetProjectCards.scala
@@ -0,0 +1,49 @@
+package io.github.manuzhang.graphql
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+object GetProjectCards extends GraphQlApp {
+
+  override def run(): Unit = {
+    val query =
+      """
+        |query {
+        |  repository(owner: "manuzhang", name: "LOG") {
+        |    project(number: 1) {
+        |      columns(first: 4) {
+        |        nodes {
+        |          name
+        |          updatedAt
+        |          cards {
+        |            nodes {
+        |              content {
+        |                ... on Issue {
+        |                  number
+        |                  title
+        |                }
+        |              }
+        |            }
+        |          }
+        |        }
+        |      }
+        |    }
+        |  }
+        |}
+        |""".stripMargin
+
+    val response = Await.result(runQuery(query), Duration.Inf)
+    val columns = response.obj("repository").obj("project").obj("columns").obj("nodes").arr
+    columns.foreach {
+      col => if (col.obj("name").str == "Today") {
+        println(col.obj("updatedAt"))
+        col.obj("cards").obj("nodes").arr.map { card =>
+          val issue = card.obj("content")
+          val number = issue.obj("number")
+          val title = issue.obj("title")
+          println(s"#$number, $title")
+        }
+      }
+    }
+  }
+}
